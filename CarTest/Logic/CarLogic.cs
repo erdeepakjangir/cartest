74e8ea62627951f087fd1865191811ab2fc5c42e
erdeepakjangir <erdeepakjangir@gmail.com>
2017-10-02 08:57:56 +0530

    added logic and model files

---
+++ CarTest/Logic/CarLogic.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CarTest.DBEntity;
+using CarTest.Models;
+namespace CarTest.Logic
+{
+    public class CarLogic
+    {
+        public List<CarModel> SearcgCarListByUser(int userID, string q)
+        {
+            List<CarModel> carList = new List<CarModel>();
+            using (var context = new CarTestEntities())
+            {
+                var allData =context.Cars.Where(w => w.UserId == userID);
+                if (!string.IsNullOrWhiteSpace(q))
+                {
+                    allData = allData.Where(w => w.Model.Contains(q) || w.Brand.Contains(q));
+                }
+                carList = allData.Select(s => new CarModel()
+                     {
+                         Brand = s.Brand,
+                         CarID = s.CarID,
+                         Model = s.Model,
+                         New = s.New,
+                         Price = s.Price,
+                         Year = s.Year,
+
+                     }).ToList();
+
+                return carList;
+            }
+
+        }
+        public CarModel GetCarDetailByUser(int carId, int userID)
+        {
+            CarModel detail = new CarModel();
+            using (var context = new CarTestEntities())
+            {
+                detail = context.Cars.Where(w => w.UserId == userID && w.CarID == carId).Select(s => new CarModel()
+                {
+                    Brand = s.Brand,
+                    CarID = s.CarID,
+                    Model = s.Model,
+                    New = s.New,
+                    Price = s.Price,
+                    Year = s.Year,
+
+                }).FirstOrDefault();
+
+                return detail;
+            }
+        }
+        public bool AddCar(CarModel model)
+        {
+            try
+            {
+                Car car = new Car();
+                car.Brand = model.Brand;
+                car.Model = model.Model;
+                car.Year = model.Year;
+                car.Price = model.Price;
+                car.New = model.New;
+                car.UserId = model.UserId;
+
+                using (var context = new CarTestEntities())
+                {
+                    context.Cars.Add(car);
+                    context.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public bool UpdateCar(CarModel model)
+        {
+            try
+            {
+                using (var context = new CarTestEntities())
+                {
+                    Car car = context.Cars.Where(w => w.CarID == model.CarID).FirstOrDefault();
+                    car.Brand = model.Brand;
+                    car.Model = model.Model;
+                    car.Year = model.Year;
+                    car.Price = model.Price;
+                    car.New = model.New;
+                    context.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public bool DeleteCar(int carId)
+        {
+            try
+            {
+                using (var context = new CarTestEntities())
+                {
+                    Car car = context.Cars.Where(w => w.CarID == carId).FirstOrDefault();
+                    context.Cars.Remove(car);
+                    context.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
