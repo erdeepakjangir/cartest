c67d5d315a2353e2db1fdf3c43ff5d40b4357931
erdeepakjangir <erdeepakjangir@gmail.com>
2017-10-03 11:56:56 +0530

    migrated to mvc5

--- CarTest/Global.asax.cs
+++ CarTest/Global.asax.cs
@@ -23,7 +23,7 @@ namespace CarTest
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
